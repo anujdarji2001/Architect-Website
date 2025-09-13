0f6ea64c72108e147baf3a858ec71f8a69685cf9
Karansinh <karan.rathod@sameeksha.capital>
2025-09-13 15:05:52 +0530

    Coming soon page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // enable static HTML export
+  basePath: process.env.NODE_ENV === "production" ? "/Architect-Website" : "",
+  images: { unoptimized: true }, // GitHub Pages doesn’t support Next.js Image Optimization
 };
 
 export default nextConfig;
