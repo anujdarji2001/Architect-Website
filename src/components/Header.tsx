69099188f97695ed363e270d9d53cb28a1c05e4f
anujdarji2001 <anujdarji2001@gmail.com>
2025-11-15 13:13:33 +0530

    Remove debug logging of current pathname in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -36,9 +36,6 @@ export default function Header() {
     return false;
   };
 
-  // Debug: Log current pathname to help identify issues
-  console.log('Current pathname:', pathname);
-
   return (
     <header className="fixed top-0 left-0 right-0 bg-white/95 backdrop-blur-md border-b border-gray-200 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
